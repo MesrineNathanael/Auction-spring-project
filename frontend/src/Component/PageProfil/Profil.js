dd525b1fc7c1b17c79b1fffe1e8d0fb2d3e4860c
Pierre Pairault <51343963+PltPierre@users.noreply.github.com>
2022-04-08 16:18:24 +0200

    Create Profil.js

---
+++ frontend/src/Component/PageProfil/Profil.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+
+
+class Profil extends Component {
+
+    render(){
+        return(
+            <div>test</div>
+        );
+    }
+}
+
+
+export default withRouter(Profil);
