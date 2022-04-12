1628a12215a38d11c951b0c4f5b45119e16e0eaa
Pierre Pairault <51343963+PltPierre@users.noreply.github.com>
2022-04-12 16:42:57 +0200

    Update Home.js

--- frontend/src/Component/Home.js
+++ frontend/src/Component/Home.js
@@ -99,7 +99,7 @@ class Home extends Component {
                                             <span className={styles.title}>{products.name}</span>
                                         </a>
                                         <span className={styles.title}>{products.name}</span>
-                                        <span className={styles.price}>Prix : {products.sellPrice} credits</span>
+                                        <span className={styles.price}>Prix : {products.basePrice} credits</span>
                                         <span className={styles.dateEnd}>Date de fin : {products.dateEnd}</span>
                                         <span className={styles.seller}>Vendeur : <Link href={'/profil/'+products.idUserSeller}>
                                         {this.state.users.map((users) => 
